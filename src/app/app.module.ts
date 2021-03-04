46fdca10aa2b1dc26d8673073ed163a64b33dbc0
s1131610 <s1131610@monmouth.edu>
2021-03-03 20:11:31 -0500

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, PortfolioComponent, EventComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
